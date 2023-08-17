0c7706cf4f994c52573fd1de7bd6a481faba6099
JatinSexy <jatin.pandey3327@protonmail.ch>
2023-08-17 19:51:46 +0530

    added matrix effect for PC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
@@ -21,7 +21,6 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
 
 
 function App() {
@@ -30,16 +29,84 @@ function App() {
 
   return (
     <div className="App">
+      <MatrixBackground />
       <header>
         <h1>🧑🏻‍💻💬</h1>
         <SignOut />
       </header>
 
       <section>
+      
         {user ? <ChatRoom /> : <SignIn />}
       </section>
 
     </div>
+  );
+}
+
+function MatrixBackground({ timeout = 50 }) {
+  const canvas = useRef();
+
+  useEffect(() => {
+      const context = canvas.current.getContext('2d');
+
+      const width = document.body.offsetWidth;
+      const height = document.body.offsetHeight;
+      canvas.current.width = width;
+      canvas.current.height = height;
+
+      context.fillStyle = '#000';
+      context.fillRect(0, 0, width, height);
+
+      const columns = Math.floor(width / 20) + 1;
+      const yPositions = Array.from({ length: columns }).fill(0);
+
+      context.fillStyle = '#000';
+      context.fillRect(0, 0, width, height);
+
+      const matrixEffect = () => {
+          context.fillStyle = '#0001';
+          context.fillRect(0, 0, width, height);
+
+          context.fillStyle = '#0f0';
+          context.font = '15pt monospace';
+
+          yPositions.forEach((y, index) => {
+              const text = String.fromCharCode(Math.random() * 128);
+              const x = index * 20;
+              context.fillText(text, x, y);
+
+              if (y > 100 + Math.random() * 10000) {
+                  yPositions[index] = 0;
+              } else {
+                  yPositions[index] = y + 20;
+              }
+          });
+      };
+
+      const interval = setInterval(matrixEffect, timeout);
+      return () => {
+          clearInterval(interval);
+      };
+  }, [canvas, timeout]);
+
+  return (
+      <div
+          style={{
+              background: '#000000',
+              overflow: 'hidden',
+              position: 'fixed',
+              height: '100%',
+              width: '100%',
+              zIndex: -1,
+              left: '0',
+              top: '0',
+          }}
+      >
+          <canvas
+              ref={canvas}
+          />
+      </div>
   );
 }
 
@@ -119,7 +186,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="avatar" />
       <p>{text}</p>
     </div>
   </>)
